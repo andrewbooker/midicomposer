cbedc7245826b6daccfc6bb8120965bf9381dbc0
Andrew Booker <improvizone@gmail.com>
2020-05-03 23:29:34 +0100

    sepecifying tempo and pulses per quarter note according to the desire time interval between notes

--- exampleMidi.py
+++ exampleMidi.py
@@ -17,18 +17,37 @@ import random
 topNotes = [59, 60, 62, 64, 66, 67, 69, 71, 72, 74]
 bassNotes = [35, 36, 38, 40]
 
+nextT = 0
+nextB = 0
 def anyIn(notes):
-    return notes[random.randint(0, len(notes) - 1)]
+    #return notes[random.randint(0, len(notes) - 1)]
+    global nextB
+    global nextT
+    if notes == bassNotes:
+        nextB += 1
+        if nextB == len(notes):
+            nextB = 0
+        return notes[nextB]
+
+    if notes == topNotes:
+        nextT += 1
+        if nextT == len(notes):
+            nextT = 0
+        return notes[nextT]
 
 m = MidiFileWriter("./test.mid")
-m.setTempo(120)
-sixteenths = 2
+intervalLength = 1000 # if tempo, =16ths otherwise =milliseconds
+
+tempo = 57
+ppqn = 60000/tempo
+
+m.setTempo(tempo, ppqn)
 
 for n in range(24):
-    rep = random.randint(2, 7)
-    m.addNote("bass", anyIn(bassNotes), 99, rep * sixteenths) 
+    rep = 4#random.randint(3, 7)
+    m.addNote("bass", anyIn(bassNotes), 99, rep * intervalLength) 
     for r in range(rep):
-        m.addNote("top", anyIn(topNotes), 99, 1 * sixteenths)
+        m.addNote("top", anyIn(topNotes), 99, 1 * intervalLength)
     
 
 del m
